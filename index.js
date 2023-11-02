a1e4a758ab27e1219a29c1fa19b01553a952decf
sofia <sofia@example.com>
2023-11-02 09:53:18 -0300

    configuracion diployt

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
  app.use(express.json());
+ app.use(cors());
 
 //defino la ruta base
 app.get("",(req,res) =>{
@@ -29,8 +31,8 @@ app.post("/api", (req,res)=>{
 
 
 //inicia la pag
-app.listen(4000, ()=>{
-    console.log("servidor activo");
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log("servidor activo", process.env.PORT||4000);
 });
 
 //le dice a la app que use esta ruta por defecto si no ingresa a la ruta base
